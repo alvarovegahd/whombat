5009b774617f8f844be5c513d8cbc2cbac69354a
mbsantiago <santiago.mbal@gmail.com>
2025-12-18 20:29:14 +0000

    Ran formatter

--- front/src/lib/components/ui/ErrorToast.tsx
+++ front/src/lib/components/ui/ErrorToast.tsx
@@ -15,11 +15,7 @@ export default function ErrorToast({
     <div className="flex flex-col gap-2">
       <div>{message}</div>
       <div className="flex gap-3">
-        <Button
-          onClick={() => onShowDetails?.()}
-          mode="text"
-          padding="p-1"
-        >
+        <Button onClick={() => onShowDetails?.()} mode="text" padding="p-1">
           Show Details
         </Button>
         <Button
