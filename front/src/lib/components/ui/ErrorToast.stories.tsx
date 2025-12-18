d9f4235d63a6e76b555af5c791bd5028d0c8f5d3
mbsantiago <santiago.mbal@gmail.com>
2025-12-18 19:20:23 +0000

    Pure ErrorToast component

--- front/src/lib/components/ui/ErrorToast.stories.tsx
+++ front/src/lib/components/ui/ErrorToast.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import type { AxiosError } from "axios";
+import { fn } from "@storybook/test";
 
 import ErrorToast from "./ErrorToast";
 
@@ -10,67 +10,18 @@ const meta: Meta<typeof ErrorToast> = {
     layout: "centered",
   },
   tags: ["autodocs"],
+  args: {
+    onShowDetails: fn(),
+    onCopy: fn(),
+  },
 };
 
 export default meta;
 
 type Story = StoryObj<typeof ErrorToast>;
 
-const mockAxiosError: AxiosError = {
-  name: "AxiosError",
-  message: "Request failed with status code 500",
-  // @ts-ignore
-  config: {},
-  isAxiosError: true,
-  toJSON: () => ({}),
-  response: {
-    data: {
-      exception: "InternalServerError",
-      traceback:
-        'Traceback (most recent call last):...\n  File "/app/main.py", line 10, in <module>\n    raise Exception("Something went wrong on the server")\nException: Something went wrong on the server',
-      detail: "Something went wrong on the server",
-    },
-    status: 500,
-    statusText: "Internal Server Error",
-    headers: {},
-    // @ts-ignore
-    config: {},
-    request: {},
-  },
-};
-
 export const Default: Story = {
   args: {
-    error: mockAxiosError,
     message: "An unexpected error occurred. Please try again later.",
-  },
-};
-
-export const NoTraceback: Story = {
-  args: {
-    error: {
-      ...mockAxiosError,
-      response: {
-        ...mockAxiosError.response,
-        data: {
-          exception: "ValidationError",
-          detail: "Invalid input provided.",
-        },
-      },
-    } as AxiosError,
-    message: "There was an issue with your input.",
-  },
-};
-
-export const GenericError: Story = {
-  args: {
-    error: {
-      ...mockAxiosError,
-      response: {
-        ...mockAxiosError.response,
-        data: {},
-      },
-    } as AxiosError,
-    message: "A generic error occurred.",
   },
 };
